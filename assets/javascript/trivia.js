32f7b57179777ce6f9a43431c708a90d774a93a6
elizajanus <32393963+elizajanus@users.noreply.github.com>
2018-03-06 10:29:26 -0500

    Add files via upload

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -1,5 +1,24 @@
-$( document ).ready(function() {
-    
+$(document).ready(function() {
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
+var clicked = 0;
+
+function score() {
+  $(".question").each(function() {
+    var submitted = $(this).find('input:checked').val();
+    if (submitted == "correct") {
+      correct++;
+      console.log(correct);
+    } else if (submitted == "incorrect") {
+        incorrect++;
+        console.log(incorrect);
+      } else if (submitted == undefined){
+        unanswered++;
+        console.log(unanswered);
+      }
+    });
+};
 
 $('#start').on('click',function(){
   
@@ -18,48 +37,27 @@ $('#start').on('click',function(){
     function decrement() {
       number--;
       $("#countdown").html("<h2> Time Remaining: " + number + "</h2>");
-      if (number === 0) {
+      if (number === 0 && clicked === 0) {
         $('#endpage').html($('#endpage').html()).show().siblings('div').hide();
-        $("#question").each(function() {
-          var submitted = $('input[name=answer]:checked').val();
-          if (submitted == "correct") {
-            correct++;
-            console.log(correct);
-          } else if (submitted == "incorrect") {
-              incorrect++;
-              console.log(incorrect);
-            } else if (submitted == undefined){
-              unanswered++;
-              console.log(unanswered);
-            }
-            $("#correct").text("Correct: " + correct);
-            $("#incorrect").text("Incorrect: " + incorrect);
-            $("#unanswered").text("Unanswered: " + unanswered);
-          });
-      }
-    }
+        score();
+        $("#correct").text("Correct: " + correct);
+        $("#incorrect").text("Incorrect: " + incorrect);
+        $("#unanswered").text("Unanswered: " + unanswered);
+      } if (number === 0 && clicked === 1) {
 
-    var correct = 0;
-    var incorrect = 0;
-    var unanswered = 0;
+      }
+    } 
+   
     $('#submit').on('click',function(){ 
-        $('#endpage').html($('#endpage').html()).show().siblings('div').hide();
-        $("#question").each(function() {
-          var submitted = $('input[name=answer]:checked').val();
-          if (submitted == "correct") {
-            correct++;
-            console.log(correct);
-          } else if (submitted == "incorrect") {
-              incorrect++;
-              console.log(incorrect);
-            } else if (submitted == undefined){
-              unanswered++;
-              console.log(unanswered);
-            }
-            $("#correct").text("Correct: " + correct);
-            $("#incorrect").text("Incorrect: " + incorrect);
-            $("#unanswered").text("Unanswered: " + unanswered);
-          });
+      clicked++;
+      console.log("clicked submit");
+      $('#endpage').html($('#endpage').html()).show().siblings('div').hide();
+      score();
+      $("#correct").text("Correct: " + correct);
+      $("#incorrect").text("Incorrect: " + incorrect);
+      $("#unanswered").text("Unanswered: " + unanswered);
+    });
+    
         
     });
   });
@@ -67,4 +65,4 @@ $('#start').on('click',function(){
 
 
 
-    });
+    
